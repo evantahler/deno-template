508a2fc8a9909f98eec279b59d6cbff665368826
Evan Tahler <evantahler@gmail.com>
2021-03-21 14:39:57 -0700

    deno init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Deno Hero
+
+## Running
+
+```
+cp env.example .env
+```
+
+then
+
+```
+./bin/start
+```
+
+## Notes
+
+- We use Import Maps
---
+++ index.ts
@@ -0,0 +1,18 @@
+import { serve } from "http/server.ts";
+import "dotenv/load.ts";
+
+const hostname = Deno.env.get("BIND");
+const port = Number(Deno.env.get("PORT"));
+
+const server = serve({ hostname, port });
+
+console.log(
+  `HTTP webserver running.  Access it at:  http://${hostname}:${port}`
+);
+
+for await (const request of server) {
+  let bodyContent = "Your user-agent is:\n\n";
+  bodyContent += request.headers.get("user-agent") || "Unknown";
+
+  request.respond({ status: 200, body: bodyContent });
+}
