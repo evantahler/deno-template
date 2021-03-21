fb7d1937f8c2494caa1ef799796412734a018f54
Evan Tahler <evantahler@gmail.com>
2021-03-21 15:47:18 -0700

    fancy logging (#2)

--- lib/server.ts
+++ lib/server.ts
@@ -1,11 +1,17 @@
-import { Application } from "oak/mod.ts";
+import { Application, Context } from "oak/mod.ts";
 import { denoEnv } from "./env.ts";
 
 export const app = new Application();
 
 app.use((ctx) => {
-  if (denoEnv() !== "test") console.log(`request from ${ctx.request.ip}`);
-
   const payload = { message: "Hello World" };
   ctx.response.body = JSON.stringify(payload);
+  log(ctx);
 });
+
+export function log(ctx: Context) {
+  if (denoEnv() === "test") return;
+  const parsed = new URL(String(ctx.request.url));
+  const message = `[${ctx.request.method}] ${ctx.request.ip} ${parsed.pathname}`;
+  console.log(message);
+}
